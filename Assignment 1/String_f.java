ceb8c0e524be0f9184e36fdf691d07e000c44041
Priyanshu <princepriyanshu.vaishnav@gmail.com>
2022-12-17 02:52:13 +0530

    Gitignore and files added

---
+++ Assignment 1/String_f.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class String_f {
+    public static void main(String args[])
+    {
+        String s="raj";
+        System.out.println(s.concat("Vaishba"));
+        Scanner sc=new Scanner(System.in);
+        sc.nextLine();
+        
+
+    }
+}
